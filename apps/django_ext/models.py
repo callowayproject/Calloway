4ce44527430b3737318114e96e26ea2a6eaafd9e
Corey Oordt <coordt@washingtontimes.com>
2010-02-25 12:13:49 -0500

    Removed the registration of Categories and Stories, as Stories already checks for it

--- apps/django_ext/models.py
+++
@@ -1,5 +0,0 @@
-from stories.models import Story
-from categories import register_m2m, register_fk
-
-register_m2m(Story)#, extra_params={'through':'StoryCategories'})
-register_fk(Story, extra_params={'related_name':'primary_category'})
