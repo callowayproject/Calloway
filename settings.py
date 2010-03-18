7070e48254d864ba8eebe3fe06ad1c4d2ff6a4a4
Jonathan Hensley <jhensley@washingtontimes.com>
2010-03-18 13:52:32 -0400

    work for positions...

--- settings.py
+++ settings.py
@@ -49,7 +49,7 @@ MEDIA_URL = 'http://127.0.0.1:8000/media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = 'http://127.0.0.1:8000/media/admin/'
+ADMIN_MEDIA_PREFIX = '/admin-media/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '3e3f)^l&&&izq3an(dc+g4-+ts%&b27%%rop)1nx_)9cl85qr8'
