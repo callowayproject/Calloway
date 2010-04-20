8cb8aef974d1c529cab0f2a5c8074ff77ae4faab
Justin Quick <justquick@gmail.com>
2010-04-20 15:06:41 -0400

    major reorganization, working now w/ fixture order

--- calloway/apps/django_ext/management/commands/e2b.py
+++ calloway/apps/django_ext/management/commands/e2b.py
@@ -6,36 +6,34 @@ from django.contrib.auth.models import User
 from django.db.models import get_model
 from categories.models import Category
 from staff.models import StaffMember
-import calloway
+from django.core.exceptions import ObjectDoesNotExist
+
+def getr(model):
+    def inner(pk):
+        try:
+            return model.objects.get(pk=pk)
+        except model.DoesNotExist:
+            return
+    if isinstance(model, basestring):
+        model = get_model(*model.split('.'))
+    return inner
+  
 
 class Command(BaseCommand):
-    
-    def story(s):
-        return s
-    
-    def entries(**kwargs):
-        return 'viewpoint.entry', {
-            'body': kwargs.pop('body'),
-            'author': kwargs.pop('author'),
-            'slug': kwargs.pop('slug'),
-            'title': kwargs.pop('title'),
-            'summary': kwargs.pop('tease'),
-            'pub_date': kwargs.pop('pub_date'),
-        }
-    
     mapping = {
         'news.story': 'stories.story',
         'weblogs.entry': 'viewpoint.entry',
+        'weblogs.blog': 'viewpoint.blog',
     }
+    order = ('staff','categories','blogs','entries','stories','images')
     
-    def handle(self, *args, **options):
-        if not len(args):
-            for arg in args:
-                self.migrate(arg)
-        else:
-            for attr in dir(self):
-                if attr.startswith('migrate_'):
-                    self.migrate(attr[8:], *getattr(self, attr)())
+    def handle(self, *apps, **options):
+        if len(apps):
+            for app in apps:
+                self.migrate(app)
+        for app in self.order:
+            print 'Migrating %s...' % app.title()
+            self.migrate(app)
 
     def get_fixture(self, name):
         adir = 'fixtures'
@@ -47,116 +45,145 @@ class Command(BaseCommand):
     def migrate(self, app):
         new_objs = []
         for obj in self.get_fixture(app):
-            m = self.mapping[obj['model']].split('.')
+            if obj['model'] in self.mapping:
+                m = self.mapping[obj['model']].split('.')
+            else:
+                m = obj['model'].split('.')
             model = get_model(*m)
-            
-            kw,rel = getattr(self, m[-1])(**obj['fields'])
+            if not model:
+                continue
+            obj['fields'].update(pk=obj['pk'])
+            try:
+                kw,rel = getattr(self, m[-1])(**obj['fields'])
+            except AttributeError:
+                kw,rel = self.default(**obj['fields'])
+            if kw == rel == None:
+                continue
             if not 'pk' in kw:
                 kw['pk'] = obj['pk']
-            #for old,new in mapping.items():
-            #    value = obj['fields'].get(old, None)
-            #    if new in filters:
-            #        value = filters[new](value)
-            #    kw[new] = value
-            #
-            #rel = {}
-            #for k in kw:
-            #    if k in related:
-            #        rel[k] = related[k](kw[k])
-            #
-            #for k in rel:
-            #    kw.pop(k)
-            #
-            #if model == StaffMember:
-            #    kw['user'] = User.objects.get_or_create(
-            #            username = 'staff-%s' % kw['pk'],
-            #            is_active=kw['is_active'],
-            #            email=kw['email'],
-            #            first_name=kw['first_name'],
-            #            last_name=kw['last_name'])[0]
-
-            o = model.objects.get_or_create(**kw)[0]
+            o = model.objects.create(**kw)
             for k,v in rel.items():
                 for i in v:
                     getattr(o, k).add(i)
             new_objs.append(o)
         return new_objs
     
-    def entries(self, **fields):
+    def default(self, **fields):
+        return fields,{}
+        
+    def staffmember(self, **fields):
+        try:
+            assert fields['email']
+            user = User.objects.get(
+                email=fields['email'],
+                first_name = fields['first_name'],
+                last_name = fields['last_name'],
+            )
+        except (User.DoesNotExist, AssertionError):
+            user = User.objects.create(
+                username = 'staff-%d' % fields['pk'],
+                email = fields['email'],
+                first_name = fields['first_name'],
+                last_name = fields['last_name'],
+            )
+
         return {
-            'body': fields.pop('body'),
-            'author': fields.pop('author'),
-            'slug': fields.pop('slug'),
-            'title': fields.pop('title'),
-            'summary': fields.pop('tease'),
-            'pub_date': fields.get('pub_date'),
-            'pub_time': fields.get('pub_date'),
+            'bio': fields['bio'],
+            'first_name': fields['first_name'],
+            'last_name': fields['last_name'],
+            'photo': fields['mugshot'],
+            'is_active': fields['is_active'],
+            'email': fields['email'],
+            'phone': fields['phone'],
+            'slug': fields['slug'],
+            'user': user
+        }, {
+            'sites': [Site.objects.get_current()],
         }
     
-    def migrate_staff(self):
-        return 'staff.staffmember', {
-                'bio': 'bio',
-                'first_name': 'first_name',
-                'last_name': 'last_name',
-                'mugshot': 'photo',
-                'is_active': 'is_active',
-                'sites': 'sites',
-                'email': 'email',
-                'phone': 'phone',
-                'slug': 'slug'
-            }, {}, {}, {
-                'sites': lambda l: map(lambda pk: Site.objects.get(pk=pk), l)
-            }
+    def category(self, **fields):
+        return {
+            'rght': fields['rght'],
+            'lft': fields['lft'],
+            'level': fields['level'],
+            'tree_id': fields['tree_id'],
+            'slug': fields['slug'],
+            'parent': fields['parent'] and getr(Category)(fields['parent']) or None,
+            'name': fields['name'],
+        }, {}
     
-    def migrate_stories(self):
-        c = lambda v: Category.objects.get(pk=v)
-        a = lambda v: StaffMember.objects.get(pk=v)
-        return 'stories.story', {
-                'headline': 'headline',
-                'pub_date': 'publish_date',
-                'story': 'body',
-                'sites': 'site',
-                'status': 'status',
-                'print_headline': 'subhead',
-                'status': 'status',
-                'tease': 'teaser',
-                'slug': 'slug',
-                'primary_category': 'primary_category',
-                'kicker':'kicker',
-                'bylines':'authors',
-                'categories': 'categories',
-            },{
-                'publish_date': lambda v: v.split()[0],
-                'site': lambda v: Site.objects.get_current(),
-                'primary_category': c,
-                'categories': lambda v: None
-            },{
-            }, {
-                'categories': lambda l: map(c, l or []),
-                'authors': lambda l: map(a, l)
+    def blog(self, **fields):
+        owners = []
+        for o in fields['authors']:
+            user = getr(User)(o)
+            try:
+                owners.append(StaffMember.objects.get(user=user))
+            except StaffMember.DoesNotExist:
+                owners.append(StaffMember.objects.create(user=user))
+        return {
+            'slug': fields['slug'],
+            'title': fields['title'],
+            'description': fields['description'] or '',
+            'tease': fields['tagline'],
+            'photo': fields['header_image'],
+            'creation_date': fields['created_date'],
+            'public': fields['display'],
+        }, {
+            'owners': owners,
+        }
+    
+    def entry(self, **fields):
+        user = getr(User)(fields.pop('author'))
+        try:
+            author = StaffMember.objects.get(user=user)
+        except StaffMember.DoesNotExist:
+            author = StaffMember.objects.create(user=user)
+        cat = filter(None, map(getr(Category), fields['categories']))
+        if len(cat):
+            cat = cat[0]
+        else:
+            cat = None
+        return {
+            'blog': getr('viewpoint.blog')(fields['blog']),
+            'body': fields['body'],
+            'author': author,
+            'slug': fields['slug'],
+            'title': fields['title'],
+            'tease': fields['summary'],
+            'pub_date': fields['pub_date'].split()[0],
+            'pub_time': fields['pub_date'].split()[1],
+            'category': cat
+        }, {}
+    
+    def story(self, **fields):
+        return {
+            'headline': fields['headline'],
+            'publish_date': fields['pub_date'].split()[0],
+            'publish_time': fields['pub_date'].split()[1],
+            'body': fields['story'],
+            'site': Site.objects.get_current(),
+            'status': fields['status'],
+            'subhead': fields['print_headline'],
+            'teaser': fields['tease'],
+            'slug': fields['slug'],
+            'primary_category': getr(Category)(fields['primary_category']),
+            'kicker': fields['kicker'],
+        }, {
+            'authors': map(getr(StaffMember), fields['bylines']),
+            'categories': map(getr(Category), fields['categories']),
         }
         
-    def migrate_images(self):
-        return 'massmedia.image', {
-                'creation_date': 'creation_date',
-                'photographer': 'author',
-                'one_off_photographer': 'one_off_author',
-                'credit': 'credit',
-                'caption': 'caption',
-                'photo': 'file',
-                'width': 'width',
-                'height': 'height',
-                'sites': 'sites',
-                'categories':'categories',
-            }
-        
-    def migrate_categories(self):
-        return 'categories.category', {
-                'slug':'slug',
-                'parent':'parent',
-                'name':'name',
-            }, {
-                'parent': lambda _: None
-            }, {
-            }
-        
+    def image(self, **fields):
+        return {
+            'creation_date': fields['creation_date'],
+            'author': fields['photographer'],
+            'one_off_author': fields['one_off_photographer'],
+            'credit': fields['credit'],
+            'caption': fields['caption'],
+            'file': fields['photo'],
+            'width': fields['width'],
+            'height': fields['height'],
+        }, {
+            'sites': [Site.objects.get_current()],
+            'categories': map(getr(Category), fields['categories']),
+        }
