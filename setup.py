75ef48901a9f1290da221f0b955425531ceb4f8f
Corey Oordt <coordt@washingtontimes.com>
2010-05-26 14:48:38 -0400

    Added the dependency links parameter for our pypi

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     install_requires = read_file('calloway/requirements.txt'),
+    dependency_links=['http://opensource.washingtontimes.com/pypi/simple/',],
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
