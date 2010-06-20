039cc1fab285cdf8efc53900d9ecce8ea5115798
Corey Oordt <coordt@washingtontimes.com>
2010-06-19 20:55:40 -0400

    Updating the style of the documentation

--- doc_src/conf.py
+++ doc_src/conf.py
@@ -104,7 +104,7 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "v%s documentation" % release
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -159,7 +159,7 @@ html_show_sourcelink = False
 #html_file_suffix = ''
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = '$$$$APP_NAME$$$$doc'
+htmlhelp_basename = 'callowaydoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -173,8 +173,8 @@ htmlhelp_basename = '$$$$APP_NAME$$$$doc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'app.tex', u'$$$$APP_NAME$$$$ Documentation',
-   u'me', 'manual'),
+  ('index', 'app.tex', u'Calloway Documentation',
+   u'calloway project', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
